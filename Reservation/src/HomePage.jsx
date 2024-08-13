778358601211234fec97c8f631b37d337b2802e4
kwrona05 <kwrona05@gmail.com>
2024-08-13 11:37:13 +0200

    Main page with links, in future with calendar

---
+++ Reservation/src/HomePage.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Nav from "./Nav";
+
+
+const HomePage = () => {
+  return(
+    <div>
+      <h1>Welcome on our website</h1>
+      <p>Please go to booking form below</p>
+      <Nav />
+    </div>
+  )
+}
+
+export default HomePage
