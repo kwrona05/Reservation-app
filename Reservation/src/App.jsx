918de2e41147938a8e247aa4d9ffc664ee28bb28
kwrona05 <kwrona05@gmail.com>
2024-08-13 11:36:05 +0200

    Add routing

--- Reservation/src/App.jsx
+++ Reservation/src/App.jsx
@@ -1,15 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Nav from "./Nav";
+import HomePage from "./HomePage";
+import BookingList from "./bookings";
+import BookingForm from "./new-booking";
 
+function App() {
+  const [bookings, setBookings] = useState([])
 
-const HomePage = () => {
+  const addBooking = (booking) => {
+    setBookings([...bookings, booking])
+
+  }
+
   return(
-    <div>
-      <h1>Welcome on our website</h1>
-      <p>Please go to booking form below</p>
-      <Nav />
-    </div>
+    <Router>
+      <Routes>
+                <Route path="/" element={<HomePage />} />
+                <Route path="/new-booking" element={<BookingForm onSubmit={addBooking} />} />
+                <Route path="/bookings" element={<BookingList bookings={bookings} />} />
+      </Routes>
+    </Router>
   )
 }
 
-export default HomePage
+export default App
