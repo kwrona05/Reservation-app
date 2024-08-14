05a5829f138371a1c130994e0df3f98e0f11ba31
kwrona05 <kwrona05@gmail.com>
2024-08-14 13:12:32 +0200

    add class

--- Reservation/src/HomePage.jsx
+++ Reservation/src/HomePage.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import Nav from "./Nav";
+import './App.css';
 
 
 const HomePage = () => {
   return(
-    <div>
+    <div className="main">
       <h1>Welcome on our website</h1>
       <p>Please go to booking form below</p>
       <Nav />
